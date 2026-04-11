ccb2da2ae6eea85eb8a9ddb023ed2ddf7ca812dd
cheqhque <vu000263@umn.edu>
2026-04-10 22:45:04 -0500

    Add files via upload

---
+++ Software/Motor Test/motortest/target/6.3.5/p2/obj/src/module_info.o.d
@@ -0,0 +1,39 @@
+c\:/Users/vu287/Coding/EE1301~3/EE1301-Final-Project/Software/MOTORT~1/motortest/target/6.3.5/p2//obj/./src/module_info.o: \
+ src/module_info.c ../../../dynalib/inc/module_info.inc \
+ ../../../dynalib/inc/module_info.h ../../../services/inc/static_assert.h \
+ c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\assert.h \
+ c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\_ansi.h \
+ c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\newlib-nano\newlib.h \
+ c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\_newlib_version.h \
+ c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\sys\config.h \
+ c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\machine\ieeefp.h \
+ c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\sys\features.h \
+ c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\lib\gcc\arm-none-eabi\10.2.1\include\stddef.h \
+ c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\lib\gcc\arm-none-eabi\10.2.1\include\stdint.h \
+ c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\stdint.h \
+ c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\machine\_default_types.h \
+ c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\sys\_intsup.h \
+ c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\sys\_stdint.h \
+ ../../../hal/inc/hal_platform.h ../../../hal/shared/platforms.h \
+ ../../../hal/src/tron/hal_platform_config.h \
+ ../../../hal/src/rtl872x/hal_platform_rtl8721x_config.h
+../../../dynalib/inc/module_info.inc:
+../../../dynalib/inc/module_info.h:
+../../../services/inc/static_assert.h:
+c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\assert.h:
+c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\_ansi.h:
+c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\newlib-nano\newlib.h:
+c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\_newlib_version.h:
+c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\sys\config.h:
+c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\machine\ieeefp.h:
+c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\sys\features.h:
+c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\lib\gcc\arm-none-eabi\10.2.1\include\stddef.h:
+c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\lib\gcc\arm-none-eabi\10.2.1\include\stdint.h:
+c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\stdint.h:
+c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\machine\_default_types.h:
+c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\sys\_intsup.h:
+c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\sys\_stdint.h:
+../../../hal/inc/hal_platform.h:
+../../../hal/shared/platforms.h:
+../../../hal/src/tron/hal_platform_config.h:
+../../../hal/src/rtl872x/hal_platform_rtl8721x_config.h:
---
+++ Software/Motor Test/motortest/target/6.3.5/p2/obj/src/newlib_stubs.o.d
@@ -0,0 +1,117 @@
+c\:/Users/vu287/Coding/EE1301~3/EE1301-Final-Project/Software/MOTORT~1/motortest/target/6.3.5/p2//obj/./src/newlib_stubs.o: \
+ src/newlib_stubs.cpp \
+ ../../../modules/shared/rtl872x/inc/user-part/newlib_stubs.inc \
+ c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\sys\types.h \
+ c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\_ansi.h \
+ c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\newlib-nano\newlib.h \
+ c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\_newlib_version.h \
+ c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\sys\config.h \
+ c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\machine\ieeefp.h \
+ c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\sys\features.h \
+ c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\sys\cdefs.h \
+ c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\machine\_default_types.h \
+ c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\lib\gcc\arm-none-eabi\10.2.1\include\stddef.h \
+ c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\machine\_types.h \
+ c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\sys\_types.h \
+ c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\sys\_stdint.h \
+ c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\machine\endian.h \
+ c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\machine\_endian.h \
+ c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\sys\select.h \
+ c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\sys\_sigset.h \
+ c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\sys\_timeval.h \
+ c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\sys\timespec.h \
+ c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\sys\_timespec.h \
+ c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\sys\_pthreadtypes.h \
+ c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\sys\sched.h \
+ c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\machine\types.h \
+ c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\c++\10.2.1\stdlib.h \
+ c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\c++\10.2.1\cstdlib \
+ c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\c++\10.2.1\arm-none-eabi\thumb\v8-m.main+fp\hard\bits\c++config.h \
+ c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\c++\10.2.1\arm-none-eabi\thumb\v8-m.main+fp\hard\bits\os_defines.h \
+ c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\c++\10.2.1\arm-none-eabi\thumb\v8-m.main+fp\hard\bits\cpu_defines.h \
+ c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\c++\10.2.1\pstl\pstl_config.h \
+ c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\stdlib.h \
+ c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\_ansi.h \
+ c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\sys\reent.h \
+ c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\sys\lock.h \
+ c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\assert.h \
+ c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\machine\stdlib.h \
+ c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\alloca.h \
+ c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\sys\_locale.h \
+ c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\c++\10.2.1\bits\std_abs.h \
+ c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\stdio.h \
+ c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\lib\gcc\arm-none-eabi\10.2.1\include\stdarg.h \
+ c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\sys\stdio.h \
+ ../../../services/inc/debug.h ../../../services/inc/service_debug.h \
+ ../../../services/inc/logging.h \
+ c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\string.h \
+ c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\strings.h \
+ c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\sys\string.h \
+ c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\lib\gcc\arm-none-eabi\10.2.1\include\stdint.h \
+ c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\stdint.h \
+ c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\sys\_intsup.h \
+ ../../../services/inc/panic.h ../../../hal/inc/hal_platform.h \
+ ../../../hal/shared/platforms.h \
+ ../../../hal/src/tron/hal_platform_config.h \
+ ../../../hal/src/rtl872x/hal_platform_rtl8721x_config.h \
+ ../../../hal/shared/program_regs.h ../../../services/inc/config.h \
+ ../../../services/inc/preprocessor.h
+../../../modules/shared/rtl872x/inc/user-part/newlib_stubs.inc:
+c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\sys\types.h:
+c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\_ansi.h:
+c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\newlib-nano\newlib.h:
+c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\_newlib_version.h:
+c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\sys\config.h:
+c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\machine\ieeefp.h:
+c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\sys\features.h:
+c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\sys\cdefs.h:
+c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\machine\_default_types.h:
+c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\lib\gcc\arm-none-eabi\10.2.1\include\stddef.h:
+c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\machine\_types.h:
+c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\sys\_types.h:
+c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\sys\_stdint.h:
+c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\machine\endian.h:
+c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\machine\_endian.h:
+c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\sys\select.h:
+c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\sys\_sigset.h:
+c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\sys\_timeval.h:
+c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\sys\timespec.h:
+c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\sys\_timespec.h:
+c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\sys\_pthreadtypes.h:
+c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\sys\sched.h:
+c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\machine\types.h:
+c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\c++\10.2.1\stdlib.h:
+c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\c++\10.2.1\cstdlib:
+c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\c++\10.2.1\arm-none-eabi\thumb\v8-m.main+fp\hard\bits\c++config.h:
+c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\c++\10.2.1\arm-none-eabi\thumb\v8-m.main+fp\hard\bits\os_defines.h:
+c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\c++\10.2.1\arm-none-eabi\thumb\v8-m.main+fp\hard\bits\cpu_defines.h:
+c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\c++\10.2.1\pstl\pstl_config.h:
+c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\stdlib.h:
+c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\_ansi.h:
+c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\sys\reent.h:
+c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\sys\lock.h:
+c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\assert.h:
+c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\machine\stdlib.h:
+c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\alloca.h:
+c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\sys\_locale.h:
+c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\c++\10.2.1\bits\std_abs.h:
+c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\stdio.h:
+c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\lib\gcc\arm-none-eabi\10.2.1\include\stdarg.h:
+c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\sys\stdio.h:
+../../../services/inc/debug.h:
+../../../services/inc/service_debug.h:
+../../../services/inc/logging.h:
+c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\string.h:
+c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\strings.h:
+c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\sys\string.h:
+c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\lib\gcc\arm-none-eabi\10.2.1\include\stdint.h:
+c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\stdint.h:
+c\:\users\vu287\.particle\toolchains\gcc-arm\10.2.1\arm-none-eabi\include\sys\_intsup.h:
+../../../services/inc/panic.h:
+../../../hal/inc/hal_platform.h:
+../../../hal/shared/platforms.h:
+../../../hal/src/tron/hal_platform_config.h:
+../../../hal/src/rtl872x/hal_platform_rtl8721x_config.h:
+../../../hal/shared/program_regs.h:
+../../../services/inc/config.h:
+../../../services/inc/preprocessor.h:
