a1b8db8e7b576eb345b5dd9922f55d4d2336fae8
doandanhh2007-hash <doandanhh2007@gmail.com>
2026-04-27 19:20:21 -0500

    Hi

---
+++ Software/BluetoothTest/bluetoothtest/target/6.3.5/p2/obj/src/user_module.o.d
@@ -0,0 +1,7 @@
+c\:/Users/vu287/Coding/EE1301~3/EE1301-Final-Project/Software/BLUETO~1/bluetoothtest/target/6.3.5/p2//obj/./src/user_module.o: \
+ src/user_module.c \
+ ../../../modules/shared/rtl872x/inc/user-part/user_module.inc \
+ ../../../dynalib/inc/dynalib.h ../../../hal/shared/platforms.h
+../../../modules/shared/rtl872x/inc/user-part/user_module.inc:
+../../../dynalib/inc/dynalib.h:
+../../../hal/shared/platforms.h:
